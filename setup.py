d2580921312753903845b0c1684205217e6d7313
nahidbinks-png <nahidbinks@gmail.com>
2026-08-04 14:14:17 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-pyTelegramBotAPI
-supabase
-flask
-yt-dlp
-requests
-beautifulsoup4
+from setuptools import setup, find_packages
+
+setup(
+    name="my_telegram_bot",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pyTelegramBotAPI",
+        "supabase",
+        "flask",
+        "yt-dlp",
+        "requests",
+        "beautifulsoup4",
+    ],
+)
